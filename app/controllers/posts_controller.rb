638c06ebed87d24ef6a09ffc3d5dfb02a88ab29b
josekim <joseph.kim85@gmail.com>
2017-08-06 21:00:33 -0500

    Finish edit/update/delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,11 +24,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find_by_id(params[:id])
+    @tags = @post.tags.map(&:name).join(", ")
   end
 
   def update
+    p "I WENT HEREHRHERHEHRHERHEHRHERHHERHEHRHERHEHR"
+    @post = Post.find_by_id(params[:id])
+    @post.update_attributes(params[:post].permit(:title,:body).to_h)
+    ApplicationHelper.update_tags_and_taggings(@post, params[:tags][:to_s])
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to new_post_path
+    end
   end
 
   def destroy
+    p params
+    Post.find_by_id(params[:id]).delete
+    redirect_to '/'
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,21 @@ module ApplicationHelper
     end
     nil
   end
+
+  def self.update_tags_and_taggings(post, tags)
+    self.delete_tags(post)
+    tags.split(", ").each do |tag|
+      new_tag = Tag.find_or_create_by(name: tag.strip)
+      post.tags << new_tag
+      # new_tagging = Tagging.create(post_id: post_id , tag_id: new_tag.id )
+    end
+    nil
+  end
+
+  def self.delete_tags(post)
+    post.taggings.each do |tagging|
+      tagging.delete
+    end
+  end
+
 end
